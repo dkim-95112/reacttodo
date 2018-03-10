b9f099dada1be43199c0a298ea7d21a4fef9e4a5
David Kim <dyk@Davids-MacBook-Air.local>
2018-03-10 00:40:05 -0700

    Initial

---
+++ app.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+debugger
+ReactDOM.render(
+    <div>hi</div>,
+    document.getElementById('root')
+);
+
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    entry: './app.tsx',
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    output: {
+        filename: 'bundle.js'
+    }
+};
